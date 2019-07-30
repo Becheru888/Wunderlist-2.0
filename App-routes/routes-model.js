5708d7a0675798399d81e1dd2fd7df4a066151e8
Becheru888 <becheru93@gmail.com>
2019-07-30 12:22:48 +0100

    Endpoints completed

--- App-routes/routes-model.js
+++ App-routes/routes-model.js
@@ -7,7 +7,11 @@ module.exports = {
     findUserById,
     findTaskById,
     addUser,
-    addTask
+    addTask,
+    userUpdate,
+    taskUpdate,
+    removeUser,
+    removeTask
 }
 
 //// ---- HELPERS FOR TASKS
@@ -23,7 +27,33 @@ function findTaskAssign() {
         .innerJoin("tasks", "users.id", "tasks.assignee_id");
 }
 
+async function addTask(task) {
+    const [id] = await db("tasks").insert(task);
 
+    return findTaskById(id);
+}
+
+function findTaskById(id) {
+    return db('tasks')
+        .select('id', 'task_name', 'assignee_id')
+        .where({ id })
+        .first();
+}
+
+
+function taskUpdate(id, changes) {
+    return db('tasks')
+        .where({ id })
+        .update(changes)
+}
+
+
+
+function removeTask(id) {
+    return db('tasks')
+        .where('id', id)
+        .del();
+}
 
 //// ---- HELPERS FOR USERS
 
@@ -32,32 +62,31 @@ function getUsers() {
     return db('users')
 }
 
-function getTasks() {
-    return db('tasks')
-}
 
 async function addUser(user) {
     const [id] = await db("users").insert(user);
 
     return findById(id);
-  }
+}
 
-  async function addTask(task) {
-    const [id] = await db("tasks").insert(task);
-
-    return findTaskById(id);
-  }
 
 function findUserById(id) {
     return db('users')
-      .select('id', 'firstName', 'lastName', 'username')
-      .where({id})
-      .first();
-  }
+        .select('id', 'firstName', 'lastName', 'username')
+        .where({ id })
+        .first();
+}
 
-  function findTaskById(id) {
-    return db('tasks')
-      .select('id', 'task_name', 'assignee_id')
-      .where({id})
-      .first();
-  }
+
+function userUpdate(id, changes) {
+    return db('users')
+        .where({ id })
+        .update(changes)
+}
+
+
+function removeUser(id) {
+    return db('users')
+        .where('id', id)
+        .del();
+}
--- App-routes/routes.js
+++ App-routes/routes.js
@@ -15,11 +15,11 @@ router.get('/users', async (_, res) => {
 });
 
 
-router.get('/users/assignments', async(_, res) =>{
+router.get('/users/assignments', async (_, res) => {
     const assignments = await DB.findTaskAssign()
-    try{
+    try {
         res.status(200).json(assignments)
-    }catch(error){
+    } catch (error) {
         res.status(500).json('Something went wrong')
     }
 })
@@ -35,14 +35,37 @@ router.get('/users/:id', async (req, res) => {
     }
 });
 
-router.post('/users/register', async(req, res) =>{
-    try{
-       const {username} = await DB.addUser(req.body)
-       res.status(200).json({message:`Welcome to the party ${username}`})
-    }catch(error){
+router.post('/users/register', async (req, res) => {
+    try {
+        const { username } = await DB.addUser(req.body)
+        res.status(200).json({ message: `Welcome to the party ${username}` })
+    } catch (error) {
         res.status(500).json(error)
     }
 })
+
+router.put('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const updateUser = req.body
+        await DB.userUpdate(id, updateUser)
+        res.status(200).json({ message: `User with the id ${id} was updated` })
+    } catch (error) {
+        res.status(500).json('something')
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await DB.removeUser(id)
+        res.status(200).json({ message: `User with the id ${id} was removed` })
+    } catch (error) {
+        res.status(500).json('Something went wrong')
+    }
+})
+
+//// ===== TASKS ======   ENDPOINT ========////
 
 router.get('/tasks', async (_, res) => {
     const getUsers = await DB.getTasks()
@@ -64,14 +87,34 @@ router.get('/tasks/:id', async (req, res) => {
 });
 
 
-router.post('/tasks/newtask', async(req, res) =>{
-    try{
-       const {task_name} = await DB.addTask(req.body)
-       res.status(200).json({message:`New task --> ${task_name} added.`})
-    }catch(error){
+router.post('/tasks/newtask', async (req, res) => {
+    try {
+        const { task_name } = await DB.addTask(req.body)
+        res.status(200).json({ message: `New task --> ${task_name} added.` })
+    } catch (error) {
         res.status(500).json(error)
     }
 })
 
+router.put('/tasks/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const updateTask = req.body
+        await DB.taskUpdate(id, updateTask)
+        res.status(200).json({ message: `Task with the id ${id} was updated` })
+    } catch (error) {
+        res.status(500).json('something')
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await DB.removeTask(id)
+        res.status(200).json({ message: `Task with the id ${id} was removed` })
+    } catch (error) {
+        res.status(500).json('Something went wrong')
+    }
+})
 
 module.exports = router;
