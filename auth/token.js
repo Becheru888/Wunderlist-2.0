c1a5dc3c5ec7430fe2f481526f9aa95a54d1c545
Becheru888 <becheru93@gmail.com>
2019-07-30 12:49:04 +0100

    authentication process config started

---
+++ auth/token.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports ={
+    generateToken
+};
+
+function generateToken(user){
+    const payload ={
+        subject: user.id,
+        username: user.username,
+        roles:['user']
+    };
+    const options = {
+        expiresIn: "1d"
+      };
+    return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
